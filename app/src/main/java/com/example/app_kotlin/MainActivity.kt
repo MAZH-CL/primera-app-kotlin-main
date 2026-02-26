7779ebe6765a911e3b637266d26f70a7aa4fadab
Maximiliano <max_zenteno@hotmail.com>
2026-02-26 20:32:43 -0300

    Se agrega QuizActivity y se actualiza el MainActivity

--- app/src/main/java/com/example/app_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/app_kotlin/MainActivity.kt
@@ -5,33 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.app_kotlin.ui.theme.AppkotlinTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,10 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppkotlinTheme {
-                Scaffold() { innerPadding ->
-                    MainMenu(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold { innerPadding ->
+                    MainMenu(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -61,7 +39,9 @@ fun MainMenu(modifier: Modifier = Modifier) {
     ) {
         Text("Mini Proyectos")
 
-        // Boton del CounterApp
+        Spacer(modifier = Modifier.height(12.dp))
+
+        // Botón Counter App
         Button(
             onClick = {
                 val intent = Intent(context, CounterAppActivity::class.java)
@@ -72,7 +52,9 @@ fun MainMenu(modifier: Modifier = Modifier) {
             Text("Counter App")
         }
 
-        // Boton del TodoApp
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Botón Todo App
         Button(
             onClick = {
                 val intent = Intent(context, TodoAppActivity::class.java)
@@ -81,6 +63,19 @@ fun MainMenu(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Todo App")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Botón Quiz App
+        Button(
+            onClick = {
+                val intent = Intent(context, QuizActivity::class.java)
+                context.startActivity(intent)
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Quiz App")
         }
     }
 }
